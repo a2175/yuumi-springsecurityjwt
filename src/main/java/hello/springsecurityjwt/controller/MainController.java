9bdb992ac6ca10317a043a932957ca93713c9daa
a2175 <as217512@gmail.com>
2024-10-31 17:14:12 +0900

    스프링 시큐리티 JWT 12 : 세션 정보
    https://www.youtube.com/watch?v=Y1p6bVrRExs&list=PLJkjrxxiBSFCcOjy0AAVGNtIa08VLk1EJ&index=12

--- src/main/java/hello/springsecurityjwt/controller/MainController.java
+++ src/main/java/hello/springsecurityjwt/controller/MainController.java
@@ -1,7 +1,13 @@
 package hello.springsecurityjwt.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RestController
 public class MainController {
@@ -9,6 +15,16 @@ public class MainController {
     @GetMapping("/")
     public String mainPage() {
 
-        return "Main Controller";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String name = authentication.getName();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+
+        String role = auth.getAuthority();
+
+        return "Main Controller " + name + " " + role;
     }
 }
